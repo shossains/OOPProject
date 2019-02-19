5b9be972c3dff7af2e85c7134716443fa3d754c2
Alex Shulzycki <a.w.shulzycki@student.tudelft.nl>
2019-02-19 16:57:10 +0100

    gson dependency, basic code for serversocket and client socket begun.

--- src/main/java/server/serverSocket.java
+++ src/main/java/server/serverSocket.java
@@ -1,4 +1,12 @@
 package server;
 
+import java.net.ServerSocket;
+
 public class serverSocket {
+
+    private ServerSocket ss;
+
+    public serverSocket(){
+        System.out.println("Server starting..");
+    }
 }
