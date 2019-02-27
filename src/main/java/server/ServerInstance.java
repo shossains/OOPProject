6ae152ecbee947a00b1eaa01f11082fe92323b17
shossain <shaansona@hotmail.com>
2019-02-27 18:19:45 +0100

    checktsyle issues resolved

--- src/main/java/server/ServerInstance.java
+++ src/main/java/server/ServerInstance.java
@@ -1,6 +1,10 @@
 package server;
 
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
 import java.net.Socket;
 
 /**
@@ -55,6 +59,7 @@ public class ServerInstance implements Runnable {
         writer.println("Hello World!");
         writer.flush();
     }
+
     public boolean isTrue() {
         return true;
     }
