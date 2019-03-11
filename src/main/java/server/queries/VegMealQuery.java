27174e5d24122705fc30a30d6096d0d8d20ec839
Alex Shulzycki <a.w.shulzycki@student.tudelft.nl>
2019-03-11 11:38:50 +0100

    skeleton code

--- src/main/java/server/queries/VegMealQuery.java
+++ src/main/java/server/queries/VegMealQuery.java
@@ -1,5 +1,11 @@
 package server.queries;
 
 public class VegMealQuery extends ServerQuery {
+    private String somethingPassedByJson;
+
+    public String runQuery(){
+        //do veg meal stuff
+        return null;
+    }
 
 }
