e330489cb60f244d226b1a5167378125a77f8887
Alex Shulzycki <a.w.shulzycki@student.tudelft.nl>
2019-03-19 12:03:14 +0100

    removed deprecated functions, completely cleaned up query.java, now runs optimized and with only one database connection. Vegmeal query implements this new system.

--- src/main/java/server/db/Query.java
+++ src/main/java/server/db/Query.java
@@ -1,6 +1,5 @@
 package server.db;
 
-import javax.xml.transform.Result;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
@@ -10,10 +9,15 @@ import java.util.ArrayList;
 public class Query extends Adapter {
 
     /**
-     * Execute any given query, as long as it is not a select query.
+     * Execute any given query.
+     *
      * @param query The queries given as string to be executed
+     * @return a ResultSet array of SELECT query results, in order corresponding to that
+     *         of the query array order.
+     *         I.E.: the first select query appears in the return array first, the second second.
+     *         Returns an empty array if no SELECT queries were specified.
      */
-    public static void query(String query[]) {
+    public static ResultSet[] query(String query[]) {
         Query db = new Query();
         db.connect();
         try {
@@ -22,10 +26,18 @@ public class Query extends Adapter {
             ArrayList<ResultSet> resultSets = new ArrayList<>();
 
             for (int i = 0; i < query.length; i++) {
-                if(query[i].contains("SELECT")){
-                    //select query, save for later
+                if (query[i].contains("SELECT")) {
+                    //select query, add resultSet to array
+                    try {
+                        PreparedStatement select = conn.prepareStatement(query[i]);
+                        ResultSet resultSet = select.executeQuery();
+                        resultSets.add(resultSet);
+                    } catch (SQLException e) {
+                        e.printStackTrace();
+                        System.out.println("Error executing SELECT query");
+                    }
 
-                }else {
+                } else {
                     //add to batch
                     try {
                         stmnt.addBatch(query[i]);
@@ -37,101 +49,10 @@ public class Query extends Adapter {
             }
             conn.commit();
             db.disconnect();
-        }catch(Exception e){ //dont fucking do this
-            e.printStackTrace();
-        }
-    }
-
-    /**
-     * Method that checks if the given email already exists in the database.
-     * @param username the email that you want to check
-     * @return true is it exists in the database
-     */
-    public boolean checkExistence(String username) {
-        try {
-            PreparedStatement st = conn.prepareStatement(
-                    "SELECT username FROM client WHERE username = '" + username + "'");
-
-            ResultSet rs = st.executeQuery();
-            while (rs.next()) {
-                String res = rs.getString(1);
-
-                if (res.equals(username)) {
-                    return true;
-                }
-            }
-
-            rs.close();
-            st.close();
+            return resultSets.toArray(new ResultSet[resultSets.size()]);
         } catch (SQLException e) {
             e.printStackTrace();
         }
-        return false;
-    }
-
-    /**
-     * Checks the amount of points a user has right now.
-     * @param username the user that you want to check
-     * @return the amount of points in JSON format
-     */
-    public static String eaten(String username) {
-        Query db = new Query();
-        db.connect();
-
-        try {
-            PreparedStatement st = conn.prepareStatement(
-                    "SELECT points FROM points WHERE username = '" + username + "'");
-
-            ResultSet rs = st.executeQuery();
-            while (rs.next()) {
-                int res = rs.getInt(1);
-                db.disconnect();
-                return "{\"points\" : " + res + "}";
-            }
-
-            rs.close();
-            st.close();
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
-        db.disconnect();
         return null;
-    }
-
-    /**
-     * Method looks up in the database how many points someone has.
-     * @param username The person you want to retrieve the information from
-     * @return the amount of points
-     */
-    public static String checkPoints(String username) {
-        Query db = new Query();
-        db.connect();
-
-        try {
-            PreparedStatement st = conn.prepareStatement(
-                    "SELECT points FROM points WHERE username = '" + username + "'");
-
-            ResultSet rs = st.executeQuery();
-            while (rs.next()) {
-                int res = rs.getInt(1);
-                db.disconnect();
-                return res+"";
-            }
-
-            rs.close();
-            st.close();
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
-        db.disconnect();
-        return null;
-    }
-
-    /**Verifies whether the username and password match
-     * @return whether or no the user is authenticated
-     */
-    private boolean authenticate(){
-
-        return false;
     }
 }
