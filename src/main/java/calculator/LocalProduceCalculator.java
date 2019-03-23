12754c19ffc56dd5af240463c285914ba21e401f
Shaan <s.hossain@student.tudelft.nl>
2019-03-24 00:26:54 +0100

    LocalProduce calc update

--- src/main/java/calculator/LocalProduceCalculator.java
+++ src/main/java/calculator/LocalProduceCalculator.java
@@ -1,26 +1,18 @@
 package calculator;
 
-import java.util.Scanner;
-
 public class LocalProduceCalculator {
 
-    public static void produce() {
-
-        Scanner input = new Scanner(System.in);
-
-        System.out.println("Kg food bought: ");
-        double number = input.nextDouble();
-
-        number = number/1000;
+    public static double produce(double weight) {
+        weight = weight/1000;
 
         //Calculate amount of kg CO2 saved.
-        double localFood = 0.232 * number;
-        double importFood = 0.560 * number;
+        double localFood = 0.232 * weight;
+        double importFood = 0.560 * weight;
 
         double result = importFood - localFood;
 
         result = Math.round(result * 100.00) / 100.00;
-        System.out.println(result + " kg CO2 saved");
+        return result;
     }
 
 
