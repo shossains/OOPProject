16ac71f5498c5329d98482147e21507923996725
Shaan <s.hossain@student.tudelft.nl>
2019-03-26 00:09:27 +0100

    Resolve conflict 2

--- src/main/java/server/queries/VegMealQuery.java
+++ src/main/java/server/queries/VegMealQuery.java
@@ -8,17 +8,15 @@ import java.sql.SQLException;
 public class VegMealQuery extends ServerQuery {
     private boolean addMeal;
     private String mealType;
+    int addPoints;
+    Double co2;
 
     /**
      * Connects to the database and executes the query to add a vegetarian meal.
-     * TODO: Cleanup, add helper functions to make it more readable.
-     * TODO: Figure out how new points can be queried in log
-     *
      * @return json-format string of the amount of points of the username
      */
     public String runQuery() {
-        int addPoints;
-        Double co2;
+
         if (mealType == null) {
             return "{'error' : true, 'reason' : 'mealType not given'}";
         }
@@ -32,22 +30,12 @@ public class VegMealQuery extends ServerQuery {
         }
 
         if (addMeal) {
-            String[] queries = new String[4];
-            queries[0] = "UPDATE points SET points = points + " + addPoints + ", last_updated = "
-                    + "CURRENT_TIMESTAMP(0) WHERE username = '" + username + "'";
+            //run queries
+            ResultSet rs = runQueries(addPoints)[0];
 
-            queries[1] = "UPDATE points SET co2 = co2 + " + co2
-                    + " WHERE username = '" + username + "'";
-
-            queries[2] = "INSERT INTO vegetarian (username, points, type, datetime, co2) values"
-                    + " ('" + username + "'," + addPoints + ",'" + mealType
-                    + "',CURRENT_TIMESTAMP(0), " + co2 + ")";
-
-            queries[3] = "SELECT points FROM points WHERE username = '" + username + "'";
-
-            //should be one function
-            ResultSet[] rsArray = Query.runQueries(queries);
-            ResultSet rs = rsArray[0];
+            if (rs == null) {
+                return "{'error' : true, 'reason' : 'Could not find user for given credentials'}";
+            }
 
             try {
                 while (rs.next()) {
@@ -62,24 +50,32 @@ public class VegMealQuery extends ServerQuery {
                 return "{'error' : true, 'reason' : 'Error parsing resultset'}";
             }
 
-        } else if (!addMeal) {
-            String[] queries = new String[1];
-            queries[0] = "SELECT count(*) FROM localProduce WHERE username = '" + username + "'";
-
-            //should be one function
-            ResultSet[] rsArray = Query.runQueries(queries);
-            ResultSet rs = rsArray[0];
-
-            try {
-                while (rs.next()) {
-                    int res = rs.getInt(1);
-                    return "{\"points\" : " + res + "}";
-                }
-            } catch (SQLException e) {
-                e.printStackTrace();
-                return "Error in resultset";
-            }
+        } else {
+            return null;
         }
-        return "{'error' : true, 'reason' : 'Error in query'}";
+    }
+
+    /**
+     * Runs db duties for the meal, as well as authentication, returns resultset array.
+     *
+     * @param pointsToBeAdded points to be added for the meal
+     * @return resultset with the current points total.
+     */
+    private ResultSet[] runQueries(int pointsToBeAdded) {
+        String[] queries = new String[4];
+        queries[0] = "UPDATE points SET points = points + " + addPoints + ", last_updated = "
+                + "CURRENT_TIMESTAMP(0) WHERE username = '" + username + "'";
+
+        queries[1] = "UPDATE points SET co2 = co2 + " + co2
+                + " WHERE username = '" + username + "'";
+
+        queries[2] = "INSERT INTO vegetarian (username, points, type, datetime, co2) values"
+                + " ('" + username + "'," + addPoints + ",'" + mealType
+                + "',CURRENT_TIMESTAMP(0), " + co2 + ")";
+
+        queries[3] = "SELECT points FROM points WHERE username = '" + username + "'";
+
+        //should be one function
+        return Query.runQueries(queries, username, password);
     }
 }
