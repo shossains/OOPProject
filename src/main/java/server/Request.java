1c2ba44f190cac438c538d17a4305bb42e60e556
Lucas van de Geer <G.vandeGeer@student.tudelft.nl>
2019-03-26 11:39:41 +0100

    Add the CombinedQuery type

--- src/main/java/server/Request.java
+++ src/main/java/server/Request.java
@@ -3,13 +3,7 @@ package server;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 
-import server.queries.BikeRideQuery;
-import server.queries.LocalProduceQuery;
-import server.queries.PublicTransportQuery;
-import server.queries.RegisterQuery;
-import server.queries.TemperatureQuery;
-import server.queries.TestQuery;
-import server.queries.VegMealQuery;
+import server.queries.CombinedQuery;
 
 /**
  * Class for parsing requests using Gson.
@@ -72,6 +66,8 @@ public class Request {
                 return temp();
             case "PublicTransport":
                 return publicTransport();
+            case "Combined":
+                return combined();
             default:
                 return "{'error' : true, 'reason' : 'Unknown type'}";
         }
@@ -110,6 +106,11 @@ public class Request {
     private String publicTransport() {
         PublicTransportQuery publicTransportQuery = buildGson(rawQuery, PublicTransportQuery.class);
         return publicTransportQuery.runQuery();
+    }
+
+    private String combined() {
+        CombinedQuery combinedQuery = buildGson(rawQuery, CombinedQuery.class);
+        return combinedQuery.runQuery();
     }
 
     /**
