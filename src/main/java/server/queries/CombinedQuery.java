bd8506717098abf02df2a842761b0909ebcc6267
Lucas van de Geer <G.vandeGeer@student.tudelft.nl>
2019-03-29 12:35:18 +0100

    Change variable names for clarity

--- src/main/java/server/queries/CombinedQuery.java
+++ src/main/java/server/queries/CombinedQuery.java
@@ -24,29 +24,29 @@ public class CombinedQuery  extends ServerQuery {
         queries[5] = "SELECT SUM (points) AS total FROM solar WHERE username = '" + username + "'";
 
         ResultSet[] rsArray = Query.runQueries(queries);
-        ResultSet rs = rsArray[0];
-        ResultSet rs1 = rsArray[1];
-        ResultSet rs2 = rsArray[2];
-        ResultSet rs3 = rsArray[3];
+        ResultSet rsVeg = rsArray[0];
+        ResultSet rsLocProd = rsArray[1];
+        ResultSet rsBike = rsArray[2];
+        ResultSet rsPubTrans = rsArray[3];
         ResultSet rsTemp = rsArray[4];
         ResultSet rsSolar = rsArray[5];
 
 
         try {
-            while (rs.next() && rs1.next() && rs2.next() && rs3.next() && rsTemp.next() && rsSolar.next()) {
-                int res = rs.getInt(1);
-                int res1 = rs1.getInt(1);
-                int res2 = rs2.getInt(1);
-                int res3 = rs3.getInt(1);
+            while (rsVeg.next() && rsLocProd.next() && rsBike.next() && rsPubTrans.next() && rsTemp.next() && rsSolar.next()) {
+                int resVeg = rsVeg.getInt(1);
+                int resLocProd = rsLocProd.getInt(1);
+                int resBike = rsBike.getInt(1);
+                int resPubTrans = rsPubTrans.getInt(1);
                 int resTemp = rsTemp.getInt(1);
                 int resSolar = rsSolar.getInt(1);
-                rs.close();
-                rs1.close();
-                rs2.close();
-                rs3.close();
+                rsVeg.close();
+                rsLocProd.close();
+                rsBike.close();
+                rsPubTrans.close();
                 rsTemp.close();
                 rsSolar.close();
-                String resStr = "{'vegPoints' : " + res  + ", 'locProdPoints' : " + res1 + ", 'bikePoints' : " + res2 + ", 'pubTransPoints' : " + res3 + ", 'tempPoints' : " + resTemp + ", 'solarPoints' : " + resSolar + "}";
+                String resStr = "{'vegPoints' : " + resVeg  + ", 'locProdPoints' : " + resLocProd + ", 'bikePoints' : " + resBike + ", 'pubTransPoints' : " + resPubTrans + ", 'tempPoints' : " + resTemp + ", 'solarPoints' : " + resSolar + "}";
                 return resStr;
             }
             return null;
