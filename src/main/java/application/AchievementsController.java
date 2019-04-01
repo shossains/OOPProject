14ede8bfbd0a16880088ab97089dd68b846604fb
Lucas van de Geer <G.vandeGeer@student.tudelft.nl>
2019-04-01 12:30:58 +0200

    Add functionality to the controller and the go methods

--- src/main/java/application/AchievementsController.java
+++ src/main/java/application/AchievementsController.java
@@ -1,25 +1,112 @@
 package application;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.ToolBar;
+import javafx.stage.Stage;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class AchievementsController implements Initializable {
+    @FXML ToolBar myToolbar;
+
+
+
     StatsController statsController = new StatsController();
     int[] points = statsController.request();
 
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-    }
-
-    public void update() {
         achievements();
     }
 
     public void achievements() {
-        if(points[1] > 1) {
+        if(points[0] > 0) {
             System.out.println("Getting started");
         }
     }
+
+    /**
+     * The general go method which will switch to a specific scene.
+     * @param fileName The name of the file where we want to go
+     * @throws IOException TThrow if file is missing/corrupted/incomplete
+     */
+    public void go(String fileName) throws IOException {
+        Parent hmParent = FXMLLoader.load(getClass().getResource("/fxml/" + fileName + ".fxml"));
+        Scene hmScene = new Scene(hmParent);
+
+        Stage window = (Stage) myToolbar.getScene().getWindow();
+        window.setScene(hmScene);
+        window.show();
+    }
+
+    /**
+     * Go to the Local Produce screen.
+     * @param actionEvent The click of the button
+     * @throws IOException Throw if file is missing/corrupted/incomplete
+     */
+    public void goLocal(ActionEvent actionEvent) throws IOException {
+        go("LocalProduce");
+    }
+
+    /**
+     * Go to the Bike screen.
+     * @param actionEvent The click of the button
+     * @throws IOException Throw if file is missing/corrupted/incomplete
+     */
+    public void goBike(ActionEvent actionEvent) throws IOException {
+        go("BikeRide");
+    }
+
+    /**
+     * Go to the Public transport screen.
+     * @param actionEvent The click of the button
+     * @throws IOException Throw if file is missing/corrupted/incomplete
+     */
+    public void goPublic(ActionEvent actionEvent) throws IOException {
+        go("PublicTransport");
+    }
+
+    /**
+     * Go to the Temperature adjustment screen.
+     * @param actionEvent The click of the button
+     * @throws IOException Throw if file is missing/corrupted/incomplete
+     */
+    public void goTemp(ActionEvent actionEvent) throws IOException {
+        go("Temperature");
+    }
+
+    /**
+     * Go to the Solar panel screen.
+     * @param actionEvent The click of the button
+     * @throws IOException Throw if file is missing/corrupted/incomplete
+     */
+    public void goSolar(ActionEvent actionEvent) throws IOException {
+        go("SolarPanels");
+    }
+
+    /**
+     * Go to the Vegetarian meal screen.
+     * @param actionEvent The click of the button
+     * @throws IOException Throw if file is missing/corrupted/incomplete
+     */
+    public void goVeg(ActionEvent actionEvent) throws IOException {
+        go("VegMeal");
+    }
+
+    /**
+     * Go to the User Stats screen.
+     * @param actionEvent The click of the button
+     * @throws IOException Throw if chart is invalid
+     */
+    public void goStats(ActionEvent actionEvent) throws IOException {
+        go("StatsPieChart");
+    }
+
 }
