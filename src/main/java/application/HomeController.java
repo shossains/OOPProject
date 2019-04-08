9b4577b4a1245490533de3b0de1e2fd5535f670f
Alex Shulzycki <a.w.shulzycki@student.tudelft.nl>
2019-04-08 10:17:12 +0200

    fixed javafx import

--- src/main/java/application/HomeController.java
+++ src/main/java/application/HomeController.java
@@ -1,14 +1,15 @@
 package application;
 
+import client.SecureClientNetworking;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 
-import java.awt.*;
 import java.io.IOException;
 
 public class HomeController {
@@ -30,7 +31,16 @@ public class HomeController {
         String password = passwordField.getText();
 
         //prepare request
-        String request = "";
+        String request = "{'type' : 'Login', 'username' : '"+username+"', 'password' : '"+password+"'}";
+        SecureClientNetworking scn = new SecureClientNetworking(User.getServerUrl());
+
+        if(scn.sendPostRequest(request).equals("{'login' : false}")){
+            //invalid login
+            System.out.println("Invalid login");
+            //do an error message
+
+            return;
+        }
 
         //set user vars
         User.setUsername(username);
