4cdebbd2bda78577b4557f8b9a3809f65355371a
Lucas van de Geer <G.vandeGeer@student.tudelft.nl>
2019-04-13 21:32:24 +0200

    Checkstyle fix

--- src/main/java/server/queries/LoginQuery.java
+++ src/main/java/server/queries/LoginQuery.java
@@ -4,18 +4,18 @@ import server.db.Query;
 
 public class LoginQuery extends ServerQuery {
 
-        /**Checks if user/pass combination is correct.
-  * @return login true for correct combination, false if incorrect.
-  */
-           public String runQuery() {
-                //construct query
-                   String[] queries = new String[0];
+    /**Checks if user/pass combination is correct.
+     * @return login true for correct combination, false if incorrect.
+     */
+    public String runQuery() {
+        //construct query
+        String[] queries = new String[0];
 
-                     if (Query.runQueries(queries, username, password) == null) {
-                       return "{'login' : false}";
-                  }
+        if (Query.runQueries(queries, username, password) == null) {
+            return "{'login' : false}";
+        }
 
-                        return "{'login' : true}";
-            }
+        return "{'login' : true}";
+    }
 }
 
